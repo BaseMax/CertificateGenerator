090b56ee50f31ccec8779f3c94d86f34e39d2fae
Max Base <maxbasecode@gmail.com>
2023-03-19 17:09:41 +0000

    update script

--- generator.py
+++ generator.py
@@ -5,22 +5,33 @@ import os
 BACKGROUND_IMG = 'certificate-background.png'
 FONT_PATH = "arial.ttf"
 OUTPUT_DIR = "output"
-NAMES = ["Ali Mohammadiyeh", "John Doe", "Jane Smith"]
+FILE_NAMES = "names.txt"
+
+
+NAMES = []
+# NAMES = ["Ali Mohammadiyeh", "John Doe", "Jane Smith", "Iman Mirazimi"]
+
+# Read the names from the file
+with open(FILE_NAMES, "r") as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            NAMES.append(line)
 
 # Check output directory exists, if not create it
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
-# Load the background image
-img = Image.open(BACKGROUND_IMG)
-
-# Get the drawing context
-draw = ImageDraw.Draw(img)
-
 # Define the font and its size
 font = ImageFont.truetype(FONT_PATH, 130)
 
 for name in NAMES:
+    # Load the background image
+    img = Image.open(BACKGROUND_IMG)
+
+    # Get the drawing context
+    draw = ImageDraw.Draw(img)
+
     # center the text based on its size and the image size
     text_width, text_height = draw.textsize(name, font)
     x = (img.width - text_width) / 2
