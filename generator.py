b7ce4d7158fabe69ee6583b1d24226ca3407689a
Max Base <maxbasecode@gmail.com>
2023-03-19 16:42:27 +0000

    start script to generate img

---
+++ generator.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+# Firstly, let's define some constants and paths
+BACKGROUND_IMG = 'certificate-background.png'
+FONT_PATH = "arial.ttf"
+OUTPUT_DIR = "output"
+NAMES = ["Ali Mohammadiyeh", "John Doe", "Jane Smith"]
+
